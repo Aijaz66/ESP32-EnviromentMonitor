ba9708bcecabfc8ff07af0db711229e626c49c3c
Aijaz66 <130418290+Aijaz66@users.noreply.github.com>
2025-05-13 13:21:14 +0500

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+
 import WeatherDataDisplay from "./weather"
 import AnalyticsPage from "./AnalyticsPage";
 
